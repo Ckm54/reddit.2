5ae079e9cd411e12c14b051de31dbb5c84bb8f9f
collo <mutaic84@gmail.com>
2023-01-30 23:12:00 +0300

    :hammer: :lipstick: update navbar search

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,14 +1,24 @@
-import { Flex } from '@chakra-ui/react'
-import React from 'react'
+import { Flex, Image } from "@chakra-ui/react";
+import React from "react";
+import SearchInput from "./SearchInput";
 
 const Navbar = () => {
   return (
-    <Flex bg={'white'} height='44px' padding={'6px 12px'}>
-      <Flex>
-        
+    <Flex bg={"white"} height="44px" padding={"6px 12px"}>
+      <Flex align={"center"}>
+        <Image src="/images/redditFace.svg" alt="logo image" height={"30px"} />
+        <Image
+          src="/images/redditText.svg"
+          alt="logo image"
+          height={"46px"}
+          display={{ base: "none", md: "unset" }}
+        />
       </Flex>
+      {/* <Directory /> */}
+      <SearchInput />
+      {/* <RightContent /> */}
     </Flex>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
