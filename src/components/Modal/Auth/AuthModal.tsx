d0eff77db275c0e0d7f15f09da01321768e395a4
collo <mutaic84@gmail.com>
2023-01-31 08:23:18 +0300

    :hammer: add state to control openness of modal

--- src/components/Modal/Auth/AuthModal.tsx
+++ src/components/Modal/Auth/AuthModal.tsx
@@ -1,3 +1,4 @@
+import { authModalState } from "@/atoms/authModalAtom";
 import {
   Button,
   Modal,
@@ -10,24 +11,27 @@ import {
   useDisclosure,
 } from "@chakra-ui/react";
 import React from "react";
+import { useRecoilState } from "recoil";
 
 const AuthModal: React.FC = () => {
-  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  const [ modalState, setModalState ] = useRecoilState(authModalState);
+
+  const handleClose = () => {
+    setModalState((prev) => ({
+      ...prev,
+      open: false,
+    }))
+  }
+
   return (
     <>
-      <Modal isOpen={isOpen} onClose={onClose}>
+      <Modal isOpen={modalState.open} onClose={handleClose}>
         <ModalOverlay />
         <ModalContent>
           <ModalHeader>Modal Title</ModalHeader>
           <ModalCloseButton />
           <ModalBody>odal body here</ModalBody>
-
-          <ModalFooter>
-            <Button colorScheme="blue" mr={3} onClick={onClose}>
-              Close
-            </Button>
-            <Button variant="ghost">Secondary Action</Button>
-          </ModalFooter>
         </ModalContent>
       </Modal>
     </>
