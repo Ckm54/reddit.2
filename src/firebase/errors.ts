3f4ed54537242ec8b7434ad1a0e6360d84a24aba
collo <mutaic84@gmail.com>
2023-02-09 14:58:58 +0300

    :hammer: update firebase error handling

--- src/firebase/errors.ts
+++ src/firebase/errors.ts
@@ -2,4 +2,5 @@ export const FIREBASE_ERRORS = {
   'Firebase: Error (auth/email-already-in-use).': "A user with that email already exists.",
   'Firebase: Error (auth/user-not-found).': "Invalid email or password.",
   'Firebase: Error (auth/wrong-password).': "Invalid email or password.",
+  'Firebase: Access to this account has been temporarily disabled due to many failed login attempts. You can immediately restore it by resetting your password or you can try again later. (auth/too-many-requests).': 'Account temporarily disabled due to many failed attempts. Try again later'
 }
