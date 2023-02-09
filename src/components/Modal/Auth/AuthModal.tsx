d9bd80afdc32a4348c78c801d3aa48205d4cb915
collo <mutaic84@gmail.com>
2023-02-09 15:01:17 +0300

    :hammer: :lipstick: update authentication

--- src/components/Modal/Auth/AuthModal.tsx
+++ src/components/Modal/Auth/AuthModal.tsx
@@ -1,4 +1,5 @@
 import { authModalState } from "@/atoms/authModalAtom";
+import { auth } from "@/firebase/clientApp";
 import {
   Button,
   Flex,
@@ -13,12 +14,14 @@ import {
   useDisclosure,
 } from "@chakra-ui/react";
 import React from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
 import { useRecoilState } from "recoil";
 import AuthInputs from "./AuthInputs";
 import OauthButtons from "./OauthButtons";
 
 const AuthModal: React.FC = () => {
   const [modalState, setModalState] = useRecoilState(authModalState);
+  const [ user, loading, error ] = useAuthState(auth);
 
   const handleClose = () => {
     setModalState((prev) => ({
@@ -26,6 +29,12 @@ const AuthModal: React.FC = () => {
       open: false,
     }));
   };
+
+  React.useEffect(() => {
+    if(user) handleClose();
+    console.log(user);
+  }, [user])
+  
 
   return (
     <>
