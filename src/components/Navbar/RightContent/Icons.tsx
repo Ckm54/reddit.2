46b399c86dcde0f66309feb0e5df30b260170da6
collo <mutaic84@gmail.com>
2023-02-09 16:05:39 +0300

    :hammer: add icons component

---
+++ src/components/Navbar/RightContent/Icons.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { BsArrowRightCircle, BsChatDots } from "react-icons/bs";
+import { GrAdd } from "react-icons/gr";
+import {
+  IoFilterCircleOutline,
+  IoNotificationsOutline,
+  IoVideocamOutline,
+} from "react-icons/io5";
+
+type Props = {};
+
+const Icons = (props: Props) => {
+  return <div>Icons</div>;
+};
+
+export default Icons;
--- src/components/Navbar/RightContent/RightContent.tsx
+++ src/components/Navbar/RightContent/RightContent.tsx
@@ -1,12 +1,12 @@
 import AuthModal from '@/components/Modal/Auth/AuthModal'
 import { auth } from '@/firebase/clientApp'
 import { Button, Flex } from '@chakra-ui/react'
-import { signOut } from 'firebase/auth'
+import { signOut, User } from 'firebase/auth'
 import React from 'react'
 import AuthButtons from './AuthButtons'
 
 type RightContentProps = {
-  user: any
+  user?: User | null;
 }
 
 const RightContent: React.FC<RightContentProps> = ({user}) => {
