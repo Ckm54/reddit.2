4651c99926e3c4fc8fc7876ca6245c475ea528e0
collo <mutaic84@gmail.com>
2023-02-15 11:09:34 +0300

    :hammer: add code to create user on google login

--- src/components/Modal/Auth/OauthButtons.tsx
+++ src/components/Modal/Auth/OauthButtons.tsx
@@ -1,11 +1,31 @@
 import { Button, Flex, Image, Text } from '@chakra-ui/react';
 import React from 'react';
 import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
-import { auth } from '@/firebase/clientApp';
+import { auth, firestore } from '@/firebase/clientApp';
+import { User } from 'firebase/auth';
+import { doc, setDoc } from 'firebase/firestore';
 
 const OauthButtons = () => {
 
-  const [ signInWithGoogle, user, loading, error ] = useSignInWithGoogle(auth);
+  const [ signInWithGoogle, userCred, loading, error ] = useSignInWithGoogle(auth);
+
+  const createUserDocument = async (user: User) => {
+    const userDocRef = doc(firestore, 'users', user.uid);
+    const newUser = {
+      uid: user.uid,
+      email: user.email,
+      displayName: user.displayName,
+      providerData: user.providerData,
+    };
+    await setDoc(userDocRef, newUser);
+  };
+
+  React.useEffect(() => {
+    if(userCred) {
+      createUserDocument(userCred.user)
+    }
+  }, [userCred])
+  
 
   return (
     <Flex direction={'column'} width='100%'>
