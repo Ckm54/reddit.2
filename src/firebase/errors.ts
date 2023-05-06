5f522d42379c6fee6feb8e292c40349887d5ac6d
collo <mutaic84@gmail.com>
2023-05-06 08:37:41 +0300

    :hammer: display error on short passsword input

--- src/firebase/errors.ts
+++ src/firebase/errors.ts
@@ -1,6 +1,10 @@
 export const FIREBASE_ERRORS = {
-  'Firebase: Error (auth/email-already-in-use).': "A user with that email already exists.",
-  'Firebase: Error (auth/user-not-found).': "Invalid email or password.",
-  'Firebase: Error (auth/wrong-password).': "Invalid email or password.",
-  'Firebase: Access to this account has been temporarily disabled due to many failed login attempts. You can immediately restore it by resetting your password or you can try again later. (auth/too-many-requests).': 'Account temporarily disabled due to many failed attempts. Try again later'
-}
+  "Firebase: Error (auth/email-already-in-use).":
+    "A user with that email already exists.",
+  "Firebase: Error (auth/user-not-found).": "Invalid email or password.",
+  "Firebase: Error (auth/wrong-password).": "Invalid email or password.",
+  "Firebase: Access to this account has been temporarily disabled due to many failed login attempts. You can immediately restore it by resetting your password or you can try again later. (auth/too-many-requests).":
+    "Account temporarily disabled due to many failed attempts. Try again later",
+  "Firebase: Password should be at least 6 characters (auth/weak-password).":
+    "Password should be at least 6 characters",
+};
