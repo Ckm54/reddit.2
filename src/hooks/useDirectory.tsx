f2273926ec287baa861a0b88b6a7eb80e6447040
collo <mutaic84@gmail.com>
2023-05-07 17:02:43 +0300

    :technologist: hook to handle menu open and close

---
+++ src/hooks/useDirectory.tsx
@@ -0,0 +1,60 @@
+import { communityState } from "@/atoms/communityAtom";
+import {
+  DirectoryMenuItem,
+  directoryMenuState,
+} from "@/atoms/directoryMenuAtom";
+import { useRouter } from "next/router";
+import React from "react";
+import { FaReddit } from "react-icons/fa";
+import { useRecoilState, useRecoilValue } from "recoil";
+
+const useDirectory = () => {
+  const [directoryState, setDirectoryState] =
+    useRecoilState(directoryMenuState);
+  const router = useRouter();
+  const communityStateValue = useRecoilValue(communityState);
+
+  const toggleMenuOpen = () => {
+    setDirectoryState((prev) => ({
+      ...prev,
+      isOpen: !directoryState.isOpen,
+    }));
+  };
+
+  const onSelectMenuItem = (menuItem: DirectoryMenuItem) => {
+    setDirectoryState((prev) => ({
+      ...prev,
+      selectedMenuItem: menuItem,
+    }));
+
+    router.push(menuItem.link);
+    if (directoryState.isOpen) {
+      toggleMenuOpen();
+    }
+  };
+
+  React.useEffect(() => {
+    const { currentCommunity } = communityStateValue;
+
+    if (currentCommunity) {
+      setDirectoryState((prev) => ({
+        ...prev,
+        selectedMenuItem: {
+          displayText: `r/${currentCommunity.id}`,
+          link: `/r/${currentCommunity.id}`,
+          imageURL: currentCommunity.imageURL,
+          icon: FaReddit,
+          iconColor: "blue.500",
+        },
+      }));
+    }
+  }, [communityStateValue, setDirectoryState]);
+
+  return {
+    directoryState,
+    toggleMenuOpen,
+    onSelectMenuItem,
+  };
+};
+
+export default useDirectory;
